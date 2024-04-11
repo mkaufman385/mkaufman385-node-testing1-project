083b59183f0bacd61282890e0adfd17b00ff6bb8
mkaufman385 <mkaufman385@gmail.com>
2024-04-11 12:50:19 -0400

    wip

--- index.js
+++ index.js
@@ -137,9 +137,11 @@ class Car {
     const milesCanDrive = this.tank * this.mpg;
     if (distance <= milesCanDrive) {
       this.odometer = this.odometer + distance;
+      this.tank = this.tank - distance / this.mpg;
       return this.odometer;
     } else {
       this.odometer = this.odometer + milesCanDrive;
+      this.tank = 0;
       return this.odometer;
     }
   }
